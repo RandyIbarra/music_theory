e14583ef514740f404d6a2982cc2514bb73ceb49
Randy Ibarra <randyosbaldo@gmail.com>
2023-12-06 23:13:29 -0600

    chore: note names refactor

--- lib/src/note.dart
+++ lib/src/note.dart
@@ -1,34 +1,4 @@
-/// All note names (Equal temperament system)
-List<String> allNotes = [
-  'C',
-  'C#',
-  'D',
-  'D#',
-  'E',
-  'F',
-  'F#',
-  'G',
-  'G#',
-  'A',
-  'A#',
-  'B',
-];
-
-/// All note names (Equal temperament system)
-List<String> allNotesFlat = [
-  'C',
-  'Db',
-  'D',
-  'Eb',
-  'E',
-  'F',
-  'Gb',
-  'G',
-  'Ab',
-  'A',
-  'Bb',
-  'B',
-];
+import 'note_names.dart';
 
 /// Dart implementation of a musical note.
 class Note {
---
+++ lib/src/note_names.dart
@@ -0,0 +1,31 @@
+/// All note names (Equal temperament system)
+List<String> allNotes = [
+  'C',
+  'C#',
+  'D',
+  'D#',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'G#',
+  'A',
+  'A#',
+  'B',
+];
+
+/// All note names (Equal temperament system)
+List<String> allNotesFlat = [
+  'C',
+  'Db',
+  'D',
+  'Eb',
+  'E',
+  'F',
+  'Gb',
+  'G',
+  'Ab',
+  'A',
+  'Bb',
+  'B',
+];
