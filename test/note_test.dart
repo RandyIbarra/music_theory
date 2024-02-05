e73d169c9b4c02297c0f52b31cf9f09c2db1f752
Randy Ibarra <randyosbaldo@gmail.com>
2024-02-04 21:40:47 -0600

    chore: test

---
+++ test/note_test.dart
@@ -0,0 +1,39 @@
+import 'package:music_theory/music_theory.dart';
+// external packages
+import 'package:test/test.dart';
+
+void main() {
+  group('Testing Note: ', () {
+    final note = Note('C');
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('Correct init name ${note.name}', () {
+      expect(note.name, 'C');
+    });
+
+    test('To sharp ${note.name}', () {
+      final naturalName = note.name[0];
+      final auxNote = Note(naturalName);
+      final sharp = auxNote + 1;
+      print(' sharp: ${sharp.name}');
+      expect(
+        sharp.isSharp(),
+        (naturalName == 'B' || naturalName == 'E') ? false : true,
+      );
+    });
+
+    test('To flat ${note.name}', () {
+      final naturalName = note.name[0];
+      final auxNote = Note(naturalName);
+      final flat = auxNote - 1;
+      print(' flatten: ${flat.name}');
+      expect(
+        flat.isFlat(),
+        (naturalName == 'C' || naturalName == 'F') ? false : true,
+      );
+    });
+  });
+}
