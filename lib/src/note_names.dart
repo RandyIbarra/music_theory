c409656d75c4c0817e05948d85be2ddbbf1c2496
Randy Ibarra <randyosbaldo@gmail.com>
2024-06-22 16:00:12 -0600

    feat: enum NoteName, getNoteFromNoteName

--- lib/src/note_names.dart
+++ lib/src/note_names.dart
@@ -1,4 +1,34 @@
-/// All note names (Equal temperament system)
+///
+/// Natural case: All note names (Equal temperament system)
+///
+enum NoteName {
+  /// C - Do
+  c,
+  cSharp,
+
+  /// D - Re
+  d,
+  dSharp,
+
+  /// E - Mi
+  e,
+
+  /// F - Fa
+  f,
+  fSharp,
+
+  /// G - Sol
+  g,
+  gSharp,
+
+  /// A - La
+  a,
+  aSharp,
+
+  /// B - Si
+  b,
+}
+
 List<String> allNotes = [
   'C',
   'C#',
@@ -14,7 +44,80 @@ List<String> allNotes = [
   'B',
 ];
 
-/// All note names (Equal temperament system)
+String getNoteFromNoteName(NoteName noteName) {
+  String name = '';
+  switch (noteName) {
+    case NoteName.c:
+      name = 'C';
+      break;
+    case NoteName.cSharp:
+      name = 'C#';
+      break;
+    case NoteName.d:
+      name = 'D';
+      break;
+    case NoteName.dSharp:
+      name = 'D#';
+      break;
+    case NoteName.e:
+      name = 'E';
+      break;
+    case NoteName.f:
+      name = 'F';
+      break;
+    case NoteName.fSharp:
+      name = 'F#';
+      break;
+    case NoteName.g:
+      name = 'G';
+      break;
+    case NoteName.gSharp:
+      name = 'G#';
+      break;
+    case NoteName.a:
+      name = 'A';
+      break;
+    case NoteName.aSharp:
+      name = 'A#';
+      break;
+    case NoteName.b:
+      name = 'B';
+      break;
+  }
+  return name;
+}
+
+///
+/// Flat case: All note names (Equal temperament system)
+///
+enum FlatNoteName {
+  /// C - Do
+  c,
+
+  /// D - Re
+  dFlat,
+  d,
+
+  /// E - Mi
+  eFlat,
+  e,
+
+  /// F - Fa
+  f,
+
+  /// G - Sol
+  gFlat,
+  g,
+
+  /// A - La
+  aFlat,
+  a,
+
+  /// B - Si
+  bFlat,
+  b,
+}
+
 List<String> allNotesFlat = [
   'C',
   'Db',
@@ -29,3 +132,46 @@ List<String> allNotesFlat = [
   'Bb',
   'B',
 ];
+
+String getNoteFromFlatNoteName(FlatNoteName noteName) {
+  String name = '';
+  switch (noteName) {
+    case FlatNoteName.c:
+      name = 'C';
+      break;
+    case FlatNoteName.dFlat:
+      name = 'Db';
+      break;
+    case FlatNoteName.d:
+      name = 'D';
+      break;
+    case FlatNoteName.eFlat:
+      name = 'Eb';
+      break;
+    case FlatNoteName.e:
+      name = 'E';
+      break;
+    case FlatNoteName.f:
+      name = 'F';
+      break;
+    case FlatNoteName.gFlat:
+      name = 'Gb';
+      break;
+    case FlatNoteName.g:
+      name = 'G';
+      break;
+    case FlatNoteName.aFlat:
+      name = 'Ab';
+      break;
+    case FlatNoteName.a:
+      name = 'A';
+      break;
+    case FlatNoteName.bFlat:
+      name = 'Bb';
+      break;
+    case FlatNoteName.b:
+      name = 'B';
+      break;
+  }
+  return name;
+}
