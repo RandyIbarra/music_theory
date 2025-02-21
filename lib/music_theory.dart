1559a93163ea2c2f0cdc98f91f22252f635426cd
Randy Ibarra <randyosbaldo@gmail.com>
2025-02-21 14:49:52 -0600

    core: update music_theory.dart

--- lib/music_theory.dart
+++ lib/music_theory.dart
@@ -5,4 +5,8 @@ export 'src/note_names.dart';
 export 'src/note.dart';
 export 'src/scale.dart';
 export 'src/chord/chord.dart';
+export 'src/chord/chord_registry.dart';
+export 'src/chord/chord_type_to_semitones_formula.dart';
+export 'src/enums/chord_type.dart';
+export 'src/functions/semitone_formula_to_chord_type.dart';
 export 'src/mode.dart';
